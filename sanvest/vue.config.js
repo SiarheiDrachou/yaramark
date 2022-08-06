ec6f23a511a3b0768371a6530d9508b20218998b
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-08-06 15:53:28 +0300

    Страница "ruxtex"

---
+++ sanvest/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    publicPath: '/objects/sanvest',
+    // publicPath: '.',
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config.plugin('html').tap(options => {
+            options[0].minify = false;
+            return options;
+        });
+    },
+}
