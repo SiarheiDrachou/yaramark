52e094d48d0c59eefe72c487487bdbeee41c127d
SiarheiDrachouAmitech <sergey@amitechgrp.com>
2022-08-06 15:53:55 +0300

    Страница "Детский дом"

---
+++ socio-pedagogical-center/src/assets/svg/SubmenuIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <svg width="13" height="8" viewBox="0 0 13 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 1L6.5 6.5L12 1" :stroke="stroke" stroke-width="1.5" stroke-linecap="round"/>
+    </svg>
+</template>
+
+<script>
+    export default {
+        props: {
+            stroke: {
+                default: 'black',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    path {
+        display: inline-block;
+        position: relative;
+        z-index: 2;
+        stroke: black;
+    }
+
+    svg {
+        transition: 0.5s linear;
+    }
+</style>
---
+++ socio-pedagogical-center/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    publicPath: '/objects/socio-pedagogical-center',
+    // publicPath: '.',
+    productionSourceMap: false,
+    chainWebpack: config => {
+        config.plugin('html').tap(options => {
+            options[0].minify = false;
+            return options;
+        });
+    },
+}
